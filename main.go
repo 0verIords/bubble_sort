5c1e33b74c1335108cb56097dccd2379f49802e8
Meder <meder33kg@gmail.com>
2019-01-21 17:46:30 +0600

    #1 implement sort descending

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func selectSort(array []int) []int {
+	for k := range array {
+		m := k
+		i := k + 1
+		for i < len(array) {
+			if array[i] < array[m] {
+				m = i
+			}
+			i++
+		}
+		t := array[k]
+		array[k] = array[m]
+		array[m] = t
+	}
+	return array
+}
+
+func bubbleSort(array []int) []int {
+	var temporary int
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] < array[j+1] {
+				temporary = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temporary
+			}
+		}
+	}
+	return array
+}
+
+func main() {
+	s := []int{2, 4, 8, 1, 0, 12, 3, 9, 5, 7, 6}
+	ss := selectSort(s)
+	fmt.Println(ss)
+
+	a := []int{2, 4, 8, 1, 0, 12, 3, 9, 5, 7, 6}
+	aa := bubbleSort(a)
+	fmt.Println(aa)
+}
