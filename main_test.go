aaa943c711c95ff50c7fa22b63b01f1fc8c6c0a1
Meder <meder33kg@gmail.com>
2019-01-21 18:25:22 +0600

    #3 Pass the test

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestSelectSort(t *testing.T) {
+	values := []int{9, 1, 2, 8, 3, 7, 4, 6, 5}
+	result := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	values = selectSort(values)
+	for i := 0; i < len(values); i++ {
+		if values[i] != result[i] {
+			t.Errorf("incorrect sort, %v != %v", values[i], result[i])
+		}
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	values := []int{9, 1, 2, 8, 3, 7, 4, 6, 5}
+	result := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	values = selectSort(values)
+	for i := 0; i < len(values); i++ {
+		if values[i] != result[i] {
+			t.Errorf("incorrect sort, %v != %v", values[i], result[i])
+		}
+	}
+}
