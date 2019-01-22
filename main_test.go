8dd42eec1b0d5f5a1a7759d7700411c62bc20415
Meder <meder33kg@gmail.com>
2019-01-22 13:47:43 +0600

    Fixed bug in tests [bubble sort]

--- main_test.go
+++ main_test.go
@@ -15,8 +15,8 @@ func TestSelectSort(t *testing.T) {
 
 func TestBubbleSort(t *testing.T) {
 	values := []int{9, 1, 2, 8, 3, 7, 4, 6, 5}
-	result := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	values = selectSort(values)
+	result := []int{1, 2, 3, 4, 5, 6, 7, 7, 9}
+	values = bubbleSort(values)
 	for i := 0; i < len(values); i++ {
 		if values[i] != result[i] {
 			t.Errorf("incorrect sort, %v != %v", values[i], result[i])
